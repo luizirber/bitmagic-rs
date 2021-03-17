10cc8288d92b30fbb9c68948dafe2c980db7b3a3
Luiz Irber <luiz.irber@gmail.com>
2021-03-17 11:05:09 -0700

    verbose debugging

--- bitmagic-sys/build.rs
+++ bitmagic-sys/build.rs
@@ -6,16 +6,26 @@ fn main() {
     //   - optimization settings
     //   - wasm
 
-    let dst = cmake::Config::new("BitMagic/lang-maps/")
+    let host = env::var("HOST").unwrap();
+    let target = env::var("TARGET").unwrap();
+
+    let host_and_target_contain = |s| host.contains(s) && target.contains(s);
+
+    let mut config = cmake::Config::new("BitMagic/lang-maps/");
+    config
         .build_target("bm-static")
         .define("BM_NO_STL", "1")
         //.define("CMAKE_EXPORT_COMPILE_COMMANDS", "1")
         //.env("CMAKE_EXPORT_COMPILE_COMMANDS", "1")
-        .very_verbose(true)
-        .build();
+        .very_verbose(true);
+    let dst = config.build();
+
     let mut path_dst = PathBuf::new();
     path_dst.push(dst);
     path_dst.push("build");
+    if host_and_target_contain("windows") && host_and_target_contain("msvc") {
+        path_dst.push(config.get_profile());
+    }
     println!("cargo:rustc-link-search=native={}", path_dst.display());
     println!("cargo:rustc-link-lib=static=bm-static");
 
