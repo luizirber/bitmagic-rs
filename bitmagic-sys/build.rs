1361d3a3b22819f2664293b8498b0c33f0509a1e
Luiz Irber <luiz.irber@gmail.com>
2021-03-23 21:05:30 -0700

    initial try on using cc instead of cmake for compiling bitmagic

--- bitmagic-sys/build.rs
+++ bitmagic-sys/build.rs
@@ -1,53 +1,25 @@
-use std::env;
-use std::fs::File;
-use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
-
 fn main() {
     // TODO: deal with
     //   - optimization settings
     //   - wasm
 
-    let host = env::var("HOST").unwrap();
-    let target = env::var("TARGET").unwrap();
+    //let host = env::var("HOST").unwrap();
+    //let target = env::var("TARGET").unwrap();
 
-    let host_and_target_contain = |s| host.contains(s) && target.contains(s);
+    //let host_and_target_contain = |s| host.contains(s) && target.contains(s);
 
-    // patch BitMagic/lang-maps/CMakeLists.txt
-    // -- start of patching
-    let cmake_config = Path::new("BitMagic/lang-maps/CMakeLists.txt");
+    let mut config = cc::Build::new();
 
-    // Open and read the file entirely
-    let mut src = File::open(&cmake_config).unwrap();
-    let mut data = String::new();
-    src.read_to_string(&mut data).unwrap();
-    drop(src); // Close the file early
-
-    // Run the replace operation in memory
-    let new_data = data.replace("\nset(CMAKE_BINARY_DIR", "\n#set(CMAKE_BINARY_DIR");
-
-    // Recreate the file and dump the processed contents to it
-    let mut dst = File::create(&cmake_config).unwrap();
-    dst.write_all(new_data.as_bytes()).unwrap();
-    // -- end of patching
-
-    let mut config = cmake::Config::new("BitMagic/lang-maps/");
     config
-        .build_target("bm-static")
-        .define("BM_NO_STL", "1")
-        //.define("CMAKE_EXPORT_COMPILE_COMMANDS", "1")
-        //.env("CMAKE_EXPORT_COMPILE_COMMANDS", "1")
-        .very_verbose(true);
-    let dst = config.build();
+        .cpp(true) // Switch to C++ library compilation.
+        .include("BitMagic/lang-maps/libbm/include")
+        .include("BitMagic/src")
+        .file("BitMagic/lang-maps/libbm/src/libbm.cpp")
+        .define("BM_NO_STL", "1");
 
-    let mut path_dst = PathBuf::new();
-    path_dst.push(dst);
-    path_dst.push("build");
-    if host_and_target_contain("windows") && host_and_target_contain("msvc") {
-        path_dst.push(config.get_profile());
-    }
-    println!("cargo:rustc-link-search=native={}", path_dst.display());
-    println!("cargo:rustc-link-lib=static=bm-static");
+    config.compile("bm");
+
+    println!("cargo:rustc-link-lib=static=bm");
 
     generate_bindings();
 }
