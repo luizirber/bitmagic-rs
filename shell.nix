07b502d8ab58c0beb8bca2d6c57a0d2e0fce16bd
Luiz Irber <luizirber@users.noreply.github.com>
2022-02-03 16:53:48 -0800

    Bump to new bitmagic version, v7.9.3 (#14)

--- shell.nix
+++ shell.nix
@@ -6,19 +6,29 @@ in
   with pkgs;
 
   pkgs.mkShell {
-    buildInputs = [
-      rustPlatform.rust.cargo
-      git
-      llvmPackages.libclang
-      cmake
-      rust-bindgen
-      cargo-watch
-      gdb
+    nativeBuildInputs = [
+      clang_13
     ];
 
-    shellHook = ''
-       # workaround for https://github.com/NixOS/nixpkgs/blob/48dfc9fa97d762bce28cc8372a2dd3805d14c633/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
-       export SOURCE_DATE_EPOCH=315532800 # 1980
-       export LIBCLANG_PATH="${llvmPackages.libclang}/lib";
-    '';
+    buildInputs = [
+      rustPlatform.rust.cargo
+      openssl
+      pkg-config
+
+      llvmPackages_13.libclang
+      llvmPackages_13.libcxxClang
+
+      cmake
+      rust-bindgen
+
+      git
+      gdb
+      cargo-watch
+    ];
+
+    BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${llvmPackages_13.libclang.lib}/lib/clang/${lib.getVersion clang}/include";
+    LIBCLANG_PATH = "${llvmPackages_13.libclang.lib}/lib";
+
+    # workaround for https://github.com/NixOS/nixpkgs/blob/48dfc9fa97d762bce28cc8372a2dd3805d14c633/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
+    SOURCE_DATE_EPOCH = 315532800; # 1980
   }
